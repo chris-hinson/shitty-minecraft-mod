bd4b9151851e8036ecfaafeeb59373331c926e1a
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2020-12-17 17:33:49 -0500

    added ender emitter and receiver blocks

---
+++ src/main/java/com/example/examplemod/EnderEmitterBlock.java
@@ -0,0 +1,12 @@
+package com.example.examplemod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class EnderEmitterBlock extends Block
+{
+    public EnderEmitterBlock()
+    {
+        super(Block.Properties.create(Material.ROCK));
+    }
+}
---
+++ src/main/java/com/example/examplemod/EnderReceiverBlock.java
@@ -0,0 +1,12 @@
+package com.example.examplemod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class EnderReceiverBlock extends Block
+{
+    public EnderReceiverBlock()
+    {
+        super(Block.Properties.create(Material.ROCK));
+    }
+}
