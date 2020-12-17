1c729bb4d57ed1b7dbeacdcd86b3562028d3ec11
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2020-12-17 17:21:14 -0500

    rebranded as Ender Redstone

--- src/main/java/com/example/examplemod/EnderRedstone.java
+++ src/main/java/com/example/examplemod/EnderRedstone.java
@@ -19,13 +19,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("wireless_redstone")
-public class ExampleMod
+@Mod("ender_redstone")
+public class EnderRedstone
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public EnderRedstone() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
