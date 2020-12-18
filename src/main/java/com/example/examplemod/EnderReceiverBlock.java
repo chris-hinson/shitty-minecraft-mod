44d2ba220e2ca0c2be3726639840045837b0eded
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2020-12-18 01:25:02 -0500

    made ender receiver emit signal at all times

--- src/main/java/com/example/examplemod/EnderReceiverBlock.java
+++ src/main/java/com/example/examplemod/EnderReceiverBlock.java
@@ -1,12 +1,34 @@
 package com.example.examplemod;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.RedstoneBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+
+import javax.annotation.Nullable;
 
 public class EnderReceiverBlock extends Block
 {
     public EnderReceiverBlock()
     {
         super(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.0f));
+    }
+
+    @Override
+    public boolean canConnectRedstone(BlockState state, IBlockReader world, BlockPos pos, @Nullable Direction side) {
+        return true;
+    }
+
+    @Override
+    public boolean canProvidePower(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getWeakPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
+        return 15;
     }
 }
